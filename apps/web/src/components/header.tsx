a5a2edf0d87b28bcf92096ed2d85f52420de9ed9
Dipanshu Rawat <dv451197@gmail.com>
2026-05-18 10:38:34 +0530

    ref: star count

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -121,7 +121,7 @@ export function Header() {
 						<Link href={SOCIAL_LINKS.github}>
 							<Button className="bg-background text-sm" variant="outline">
 								<HugeiconsIcon icon={GithubIcon} className="size-4" />
-								48k+
+								51k+
 							</Button>
 						</Link>
 						<Link href="/projects">
